373c728d27dbafa63551b581baac128f42584c90
Sahanahs27 <sahanahsxworkz@gmail.com>
2023-08-21 12:14:54 +0530

    commiting example through Bluetooth

---
+++ Bluetooth.java
@@ -0,0 +1,23 @@
+class Bluetooth
+{
+public static void main(String bluetooth[])
+{
+	String name = "boAt Airpodes 141 bluetooth";
+	int price = 1000;
+	String earPlacement = "Inear";
+	String controlType = "voice control";
+	String compatibleDevices = "bluetooth device";
+	String frequencyResponse = "20000Hz";
+	String controlMethod = "Touch";
+	String noiseControl = "active noise cancellation";
+    System.out.println("the name of the bluetooth " +name);
+    System.out.println("price of bluetooth " +price);
+    System.out.println("earplcements of the bluetooth " +earPlacement);
+	System.out.println("controlType of the bluetooth " +controlType);
+    System.out.println("compatibledevices of the bluetooth " +compatibleDevices);
+    System.out.println(" frequency response of the bluetooth " + frequencyResponse);
+	System.out.println(" controlMethod of the bluetooth " +controlMethod);
+	System.out.println(" noiseControl of the bluetooth " +controlMethod);
+	
+}
+}
